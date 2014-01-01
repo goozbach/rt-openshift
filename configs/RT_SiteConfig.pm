71679a86d5cd7e3023fbd5053ad6d2d80a646a46
Derek Carter <derek@goozbach.com>
2013-12-31 20:45:53 -0500

    fixing cross-site for defaults

--- configs/RT_SiteConfig.pm
+++ configs/RT_SiteConfig.pm
@@ -1,26 +1,4 @@
-# Any configuration directives you include  here will override 
-# RT's default configuration file, RT_Config.pm
-#
-# To include a directive here, just copy the equivalent statement
-# from RT_Config.pm and change the value. We've included a single
-# sample value below.
-#
-# This file is actually a perl module, so you can include valid
-# perl code, as well.
-#
-# The converse is also true, if this file isn't valid perl, you're
-# going to run into trouble. To check your SiteConfig file, use
-# this command:
-#
-#   perl -c /path/to/your/etc/RT_SiteConfig.pm
-#
-# You must restart your webserver after making changes to this file.
-
-Set( $rtname, 'example.com');
-
-# You must install Plugins on your own, this is only an example
-# of the correct syntax to use when activating them.
-# Plugin( "RT::Extension::QuickDelete" );
-# Plugin( "RT::Extension::CommandByMail" );
+Set( $rtname, '__HOSTNAME__' );
+Set( @ReferrerWhitelist, qw(__REFERRERS__) );
 
 1;
